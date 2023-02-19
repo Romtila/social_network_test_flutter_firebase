99b92561b69e02db2ea9ab5481359bc62d59a310
Roman Ugrin <romtila@yandex.ru>
2023-02-19 14:02:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# social_network_test_flutter_firebase
+ 
---
+++ android/app/src/main/kotlin/com/example/social_network_test_flutter_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.social_network_test_flutter_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
